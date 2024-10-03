372f05b835ea3c71675a71fb03ee9745cbb6b568
assilva@apoioprodesp.sp.gov.br <assilva@apoioprodesp.sp.gov.br>
2024-10-02 23:55:30 -0300

    fetching article from database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
-import articles from '@/data/articles.json';
+// import articles from '@/data/articles.json';
+import Article from '@/libs/database/Articles';
 import Image from 'next/image';
 
-export default function Home() {
+export default async function Home() {
+
+  // const highlightedArticles = await Article.get({ limite: 4 });
+  // const articles = await Article.get({ limite: 10, offset: 4 });
+  const articles = await Article.get({ orderBy: { publishedAt: 'desc' }, limite: 14 });
+  const highlightedArticles = articles.slice(0,4);
+  const listArticles = articles.slice(4);
+  // console.log('home: ', articles);
+
+
   return (
     <div className="ml-72">
       <div className="w-full h-[35vh] bg-orange-400 flex-center">
@@ -10,7 +20,7 @@ export default function Home() {
 
       <div className="container mx-auto my-6">
         <div className="grid grid-cols-4 gap-4 h-[35vh]">
-          {articles.splice(-4).map((article) => {
+          {highlightedArticles.map((article) => {
             return (
             <div key={article.title} className='flex-center relative overflow-hidden'>
               <div className='h-full w-full'>
@@ -18,10 +28,10 @@ export default function Home() {
                         src={`/assets/images/articles/${article.image}`} alt={article.title} width={600} height={400} />
               </div>
               <p className='absolute bottom-0 pt-6 pb-2 px-2 bg-gradient-to-t from-slate-900 via-slate-800 to-transparent w-full'>
-                {article.title}
+              {article.id}-{article.title}
               </p>
             </div>
-            );
+            )
           })}
 
           {/* <div className="flex-center bg-indigo-500">A</div>
@@ -37,7 +47,7 @@ export default function Home() {
           <div className="col-span-8  flex flex-col gap-4">
 
             {
-            articles.map((article) => {
+            listArticles.map((article) => {
                 return(
                   <div key={article.title} className="flex bg-slate-800 rounded-md py-4">
                     <div className='flex items-center'>
@@ -50,14 +60,15 @@ export default function Home() {
                     </div>
 
                     <div className="w-full flex flex-col gap-2 pl-4">
-                      <h2 className="text-3xl mb-4 text-indigo-400">{article.title}</h2>
+                      <h2 className="text-3xl mb-4 text-indigo-400">{article.id}-{article.title}</h2>
+                      <p className="flex-grow">{article.publishedAt.toISOString()}</p>
                       <p className="flex-grow">{article.excerpt}</p>
                       <button className='bg-slate-700 hover:bg-indigo-400/40 rounded-lg px-4 py-2 inline max-w-max'>Ler mais</button>
                     </div>
                   </div>
-                );
+                )
               })
-            };
+            }
 
           </div>
           <div className="col-span-4 bg-purple-500">
@@ -68,5 +79,5 @@ export default function Home() {
       </div>
 
     </div>
-  );
+  )
 }
