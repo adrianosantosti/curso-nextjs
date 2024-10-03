c928843962296946045251c7879882fa035a2912
assilva@apoioprodesp.sp.gov.br <assilva@apoioprodesp.sp.gov.br>
2024-10-02 21:08:23 -0300

    Import article data from json file.

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,9 +1,42 @@
 import { PrismaClient } from '@prisma/client';
+import articles from "../../src/data/articles.json";
+import { slugify } from '../../src/helpers/slugify';
 
 const prisma = new PrismaClient();
 
 async function main() {
-    console.log("sss");
+    console.log("Iniciando seed");
+    await seedArticles();
+}
+
+async function seedArticles()
+{
+    const isDev = process.env.NODE_ENV == 'development';
+
+    if (!isDev)
+    {
+        throw new Error('NODE_ENV is not a development environment');
+    }
+    
+    // delete all records
+    // truncate
+    await prisma.article.deleteMany();
+    await prisma.$executeRawUnsafe("DELETE FROM SQLITE_SEQUENCE WHERE name=$1;", "Article");
+
+    for (let article of articles){
+        const record = await prisma.article.create({
+            data: {
+                title: article.title,
+                slug: slugify(article.title),
+                excerpt: article.excerpt,
+                content: article.content,
+                image: article.image,
+                publishedAt: new Date(article.publish_date)
+            }
+        });
+
+        console.log('*** created article', record.id, record.title);
+    }
 }
 
 main().then(async() => {
