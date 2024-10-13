cead0b9552988ddc59026242fbdff9705986578d
assilva@apoioprodesp.sp.gov.br <assilva@apoioprodesp.sp.gov.br>
2024-10-12 22:38:18 -0300

    ajustes4

--- src/components/Navbar/types.d.ts
+++ src/components/Navbar/types.d.ts
@@ -1,6 +1,6 @@
 import { User } from "@prisma/client";
 
-export type NavbarProps = React.ComponentProps<"nav"> & { user?: User };
+export type NavbarProps = React.ComponentProps<"nav"> & { user?: JWTPayload };
 export type NavbarListProps = React.ComponentProps<"ul">;
 export type NavbarListItemProps = React.ComponentProps<"li">;
 export type NavbarLisItemLinkProps = React.ComponentProps<typeof Link>;
