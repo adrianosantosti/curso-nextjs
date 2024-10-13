74d4bb0646d3c7e9582677766078908f658f5cbd
assilva@apoioprodesp.sp.gov.br <assilva@apoioprodesp.sp.gov.br>
2024-10-12 22:17:12 -0300

    ajustes types2

--- src/helpers/jwt.ts
+++ src/helpers/jwt.ts
@@ -1,9 +1,9 @@
-import { SignJWT, jwtVerify } from 'jose';
+import { JWTPayload, SignJWT, jwtVerify } from 'jose';
 const JWT_Secret = process.env.JWT_SECRET;
 
 const secretKey = new TextEncoder().encode(JWT_Secret);
 
-export const encrypt = async (payload: any) => {
+export const encrypt = async (payload: JWTPayload) => {
 
     const result = await new SignJWT(payload)
     .setProtectedHeader({alg: "HS256" })
